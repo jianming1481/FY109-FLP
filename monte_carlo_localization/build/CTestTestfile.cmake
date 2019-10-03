391fd573544997fabce066821c6e2d358e7ab1f0
ChienMing Lin <jianming1481@gmail.com>
2019-10-03 14:09:36 +0800

    try to ignore all the build folder

--- monte_carlo_localization/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lui/catkin_ws/src/FY109-FLP/monte_carlo_localization
-# Build directory: /home/lui/catkin_ws/src/FY109-FLP/monte_carlo_localization/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- monte_carlo_localization/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/lui/catkin_ws/src/FY109-FLP/monte_carlo_localization/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
